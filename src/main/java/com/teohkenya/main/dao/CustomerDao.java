1fab58f99fe88e791c8fa9079c5cad091e6e54eb
KEN20960 <muchirinephat5@gmail.com>
2022-09-15 15:38:20 +0300

    I'm too lazy to insert a comment

--- src/main/java/com/teohkenya/main/dao/CustomerDao.java
+++ src/main/java/com/teohkenya/main/dao/CustomerDao.java
@@ -1,6 +1,7 @@
 package com.teohkenya.main.dao;
 
 import com.teohkenya.main.model.Customer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,12 +12,23 @@ import java.util.stream.IntStream;
  * @ Author NMuchiri
  **/
 @Component
+@Slf4j
 public class CustomerDao {
 
     public List<Customer> getCustomers(){
         return IntStream.rangeClosed(1, 50)
+                .peek(CustomerDao::sleepThread)
                 .peek(i -> System.out.println("Processing Count: " + i))
                 .mapToObj(i -> new Customer(i, "customer " + i))
                 .collect(Collectors.toList());
     }
+
+    private static void sleepThread(int milliseconds){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            log.error("InterruptedException: {}", e.getMessage());
+        }
+    }
+
 }
